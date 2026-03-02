c657aa05c8c019449efabe0e221593b4fe427c5d
Lucas Souza de Oliveira Rios <lucasriosdev@gmail.com>
2026-03-02 10:46:32 -0300

    Adicionando e configurando icon do bitcoin no framd de cima.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 # cores -------
 
@@ -15,6 +16,8 @@ janela.title('')
 janela.geometry('320x350')
 janela.configure(bg=fundo)
 
+
+# divivindo a jenela em 2 frames --------
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=157)
 
 frame_cima = Frame(janela, width=320, height=50, bg=co1, pady=0, padx=0, relief='flat')
@@ -22,6 +25,13 @@ frame_cima.grid(row=1, column=0)
 
 frame_baixo = Frame(janela, width=320, height=300, bg=fundo, pady=0, padx=0, relief='flat')
 frame_baixo.grid(row=2, column=0, sticky=NW)
+
+# Configurando o frame de cima --------
+imagem = Image.open('images/bitcoin_94576.png')
+imagem = imagem.resize((30,30), Image.ANTIALIAS)
+imagem = ImageTk.PhotoImage(imagem)
+
+
 
 
 janela.mainloop()
