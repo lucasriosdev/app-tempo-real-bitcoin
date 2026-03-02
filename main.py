9569d6c6929e132d4576f61c889e8771aa68da73
Lucas Souza de Oliveira Rios <lucasriosdev@gmail.com>
2026-03-02 10:26:54 -0300

    iniciando o aplicativo importando o tkinter e inciando a janela

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+
+# cores -------
+
+co0 = "#444466"  # preta / black
+co1 = "#feffff"  # branca / white
+co2 = "#6f9fbd"  # azul / blue
+
+fundo = "#484f60" #background
+
+janela = Tk()
+janela.title('')
+janela.geometry('320x350')
+janela.configure(bg=fundo)
+
+janela.mainloop()
